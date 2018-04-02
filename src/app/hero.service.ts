6b1af5631e8800e680aa9c35147f92f362b9f327
Sakura210 <zhanghua8286@gmail.com>
2018-04-02 22:04:09 +0800

    test3

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 @Injectable()
 export class HeroService {
 
   constructor() { }
   getHeroes():Observable<Hero[]>{
-    return of(HEROES);
+    return Observable.of(HEROES);
   }
 }
